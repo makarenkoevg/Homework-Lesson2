3e9035cbfe37a3d8d295f6a629753b5da4e7211f
makarenkoevg <makarenkoevg@mail.ru>
2022-12-12 17:14:39 +0500

    End

--- 10/Program.cs
+++ 10/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Write("Введите трехзначное число:");
+int num = int.Parse(Console.ReadLine());
+Console.Write($"Вторая цифра этого числа: {(num/10)%10}");
